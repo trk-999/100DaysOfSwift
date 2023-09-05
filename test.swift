4d15a17e5879818c8eb27401efb89540a6ddb49c
trk-999 <97623216+trk-999@users.noreply.github.com>
2023-09-04 21:50:19 -0400

    Create test.swift
    Test file that I'll regularly update as I do the test portions of the course.

---
+++ test.swift
@@ -0,0 +1,150 @@
+import Cocoa
+
+
+/*
+ 
+ Variables
+ ----------------------------------------------------
+ 
+ Question 1/6: Which of these lines of code create new variables?
+ 
+ Hint: Click to show.
+ 
+ Option 1:
+
+ message = "Good morning, Dave!"
+ 
+ Option 2:
+
+ var operatingSystem = "macOS"
+ 
+ Question 2/6: Which of these lines of code create new variables?
+
+
+ Option 1:
+
+ var address = "321 Park Lane"
+ 
+ Option 2:
+
+ favoriteColor = "heliotrope"
+ 
+ Question 3/6: Which of these lines of code create new variables?
+
+
+ Option 1:
+
+ var dinosaur = "T-Rex"
+ Option 2:
+
+ city = "Paris"
+ 
+ Question 4/6: Which of these lines of code create new variables?
+ 
+
+ Option 1:
+
+ var pirate = "LeChuck"
+ 
+ Option 2:
+
+ spaceship = "Nostromo"
+ 
+ Question 5/6: Which of these lines of code create new variables?
+
+
+ Option 1:
+
+ status = "success"
+ 
+ Option 2:
+
+ var name = "Sophie"
+ 
+ Question 6/6: Which of these lines of code create new variables?
+
+
+ Option 1:
+
+ var str = "Hello, playground"
+ 
+ Option 2:
+
+ user = "twostraws"
+ 
+ */
+
+/*
+
+Constants
+----------------------------------------------------
+
+ Question 1/6: Which of these lines create constants?
+
+
+ Option 1:
+
+ height = 172.1
+ 
+ Option 2:
+
+ let width = 14.0
+ 
+ Question 2/6: Which of these lines create constants?
+ 
+
+ Option 1:
+
+ let username = "twostraws"
+ 
+ Option 2:
+
+ var status = "success"
+ 
+ Question 3/6: Which of these lines create constants?
+ 
+
+ Option 1:
+
+ let tweeted = false
+ 
+ Option 2:
+
+ var isRead = true
+ 
+ Question 4/6: Which of these lines create constants?
+ 
+
+ Option 1:
+
+ let password = "fr0sti3s"
+ 
+ Option 2:
+
+ var season = "summer"
+ 
+ Question 5/6: Which of these lines create constants?
+ 
+
+ Option 1:
+
+ loggedIn = false
+ 
+ Option 2:
+
+ let enabled = true
+ 
+ Question 6/6: Which of these lines create constants?
+ 
+
+ Option 1:
+
+ let age = 26
+ 
+ Option 2:
+
+ var score = 1000
+ 
+*/
+ 
+var greeting = "Hello, playground"
