d17ddae7d46f738e8574fb084998350df8b0fddd
trk-999 <97623216+trk-999@users.noreply.github.com>
2023-09-14 23:37:16 -0400

    Create checkpoint1.swift
    First checkpoint in https://www.hackingwithswift.com/100/swiftui

---
+++ checkpoint1.swift
@@ -0,0 +1,15 @@
+/*
+ First checkpoint in learning swift. An
+ unfortunate hiatus occurred as I was
+ learning that had a 3 DAY delay in the
+ 100 day learning course. This is the
+ first attempt after not touch swift code
+ for a bit.
+ */
+
+import Cocoa
+
+let temperature = 32.5
+var conversion = temperature * 9 / 5 + 32
+
+print("The current celsius temperature is \(temperature)°C while in Farenheit it is \(conversion)°F")
