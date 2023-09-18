b37556acac01b1197bb1cd72bb5170a0af2a9e41
trk-999 <97623216+trk-999@users.noreply.github.com>
2023-09-17 22:53:58 -0400

    Create enum.swift
    Learning about enumerations.

---
+++ enum.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+/*
+ Create and use enumerations for sorting data
+ */
+
+enum weekday {
+    case Monday, Tuesday, Wednesday, Thursday, Friday
+}
+var day = weekday.Tuesday
+
+
+
+print(day)
